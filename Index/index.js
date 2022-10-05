9bc4a03ff81ec1662e54c5f7072783bfe28636c1
Eleyna0 <88134@glr.nl>
2022-10-05 12:21:03 +0200

    HEHEHEHE

---
+++ Index/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".btn1").click(function(){
+      $(".tekst").slideUp();
+    });
+    $(".btn2").click(function(){
+        $(".tekst").slideDown();
+    });
+});
